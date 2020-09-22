2c80d8062849d45c06332c3effa292381220ac01
Daria Jerjomina <dariarigger@gmail.com>
2020-09-21 20:36:30 -0700

    Input Field with Scroll Added
    Changed the way scale is being calculated/applied (didn't really need to change, could have used scaleKeyframe); changed UI to have an input field with an option to scroll instead of a scale button.

--- scale_frames.py
+++ scale_frames.py
@@ -1,57 +1,89 @@
 import maya.cmds as mc
 
-def main (is_first=True, are_selected_frames=True, scale_amount=1, is_vertical=True):
+# Scaling of frames based on selection pivoting from the first or the last selected frame
+
+def main (scale_factor=1, are_selected_frames=True, is_vertical=True, is_first=True):
     """
-    Scales all selected keyframes.
+    :param scale_factor: the amount we are scaling by.
+    :return:
+    """
+    anim_curve_info_dict = _get_all_keyframe_info(are_selected_frames=are_selected_frames, is_first=is_first)
+    if not anim_curve_info_dict:
+        return
+    for anim_curve, keyframe_info in anim_curve_info_dict.iteritems():
+        time_pivot = keyframe_info[0]
+        value_pivot = keyframe_info[1]
+        keyframe_indices = keyframe_info[2]
+
+        for i in range(1, len(keyframe_indices)):
+            if is_vertical:
+                # In case we are scaling vertically, the values will be changed.
+                value = mc.keyframe(anim_curve, query=True,
+                                    valueChange=True, index=(keyframe_indices[i], keyframe_indices[i]))[0]
+                new_value = (value-value_pivot)*scale_factor
+                if new_value == 0:
+                    pass
+
+                mc.keyframe(anim_curve, edit=True,
+                            valueChange=value_pivot+new_value,
+                            index=(keyframe_indices[i], keyframe_indices[i]))
+            else:
+                # In case we are scaling horizontally, the times will be changed.
+                time = mc.keyframe(anim_curve, query=True,
+                                    timeChange=True,
+                                    index=(keyframe_indices[i], keyframe_indices[i]))[0]
+
+                new_value = time_pivot+(time-time_pivot)*scale_factor
+                try:
+                    mc.keyframe(anim_curve, edit=True,
+                                timeChange=new_value,
+                                index=(keyframe_indices[i], keyframe_indices[i]))
+                except RuntimeError:
+                    # keyframes can't go past the point where they have the same time
+                    pass
+
+
+def _get_all_keyframe_info (are_selected_frames=True, is_first=True):
+    """
     :param is_first: determines whether keyframes are scaled after the first or before last.
     :param are_selected_frames: whether to scale all keyframes or only selected ones
-    :param scale_amount: how much to scale by
     """
+    anim_curve_info_dict = {}
     anim_curves = mc.keyframe(q=True, selected=True, name=True)
     if not anim_curves:
         mc.warning("Please select at least one animation curve.")
         return
     for anim_curve in anim_curves:
-        scale_frames_on_curve(anim_curve, is_first, are_selected_frames, scale_amount, is_vertical=is_vertical)
+        anim_curve_info_dict[anim_curve] = _get_single_curve_info(anim_curve, are_selected_frames, is_first)
+    return anim_curve_info_dict
 
-def scale_frames_on_curve(anim_curve, is_first=True, are_selected_frames=True,
-                          scale_amount=1, is_vertical=True):
+
+def _get_single_curve_info(anim_curve, are_selected_frames, is_first):
     """
-    Scales keyframes on a specific anim_curve based on selected ones.
-    :param anim_curve: the name of the animation curve where selected keys are placed.
-    :param is_first: determines whether keyframes are scaled after the first or before last.
-    :param are_selected_frames: whether to scale all keyframes or only selected ones
-    :param scale_amount: how much to scale by
+    :param anim_curve:  The name of a curve that we are getting info from. ex: Object_translateX
+    :return: time_pivot (time of either first or last frame), value_pivot (value of either first or
+    last frame), selected_indices (indices
     """
     selected_frame_times = mc.keyframe(anim_curve, q=True, selected=True, timeChange=True)
     selected_frame_values = mc.keyframe(anim_curve, q=True, selected=True, valueChange=True)
-    all_times = mc.keyframe(anim_curve, q=True, timeChange=True)
+    selected_indices = mc.keyframe(anim_curve, q=True, selected=True, indexValue=True)
+    all_indices = mc.keyframe(anim_curve, q=True, indexValue=True)
 
+    # Calculating values based on provided parameters and selected keyframes
+    #
     if is_first:
         time_pivot = selected_frame_times[0]
         value_pivot = selected_frame_values[0]
-        time_start = selected_frame_times[0]
-        time_end = selected_frame_times[-1]
+        index_start = selected_indices[0]
         if not are_selected_frames:
-            time_end = all_times[-1]
+            selected_indices = all_indices[index_start:]
 
     else:
-        time_start = selected_frame_times[-1]
         time_pivot = selected_frame_times[-1]
         value_pivot = selected_frame_values[-1]
-        time_end = selected_frame_times[0]
+        index_start = selected_indices[-1]
         if not are_selected_frames:
-            time_end = all_times[0]
+            selected_indices = all_indices[:index_start]+[index_start]
+            selected_indices.reverse()
 
-    if is_vertical:
-        mc.scaleKey(anim_curve, time=(time_start, time_end),
-                    timePivot=time_pivot, valuePivot=value_pivot,
-                    valueScale=scale_amount)
-    else:
-        mc.scaleKey(anim_curve, time=(time_start, time_end),
-                    timePivot=time_pivot, valuePivot=value_pivot,
-                    timeScale=scale_amount)
-
-
-
-    
+    return time_pivot, value_pivot, selected_indices
--- scale_frames_ui.py
+++ scale_frames_ui.py
@@ -8,7 +8,17 @@ from PySide2.QtUiTools import *
 from shiboken2 import wrapInstance
 
 import scale_frames
-reload(scale_frames)
+
+# UI for keyframe scaling tool. In it we can specify whether we are scaling only selected frames, or
+# all the frames that go either after the first or before the last; whether we scale vertically (by
+# value) or horizontally (by time); and whether the pivot should be at the first or the last frame.
+# The value is being changed in the input box - an instance of CustomSpinBox. The user can change
+# that value either by inserting a new value or by middle mouse dragging. Qt's SpinBox doesn't have
+# a dragging functionality, so a CustomSpinBox(QLineEdit) is created.
+# The scale factor is being calculated on value change and is new inserted value devided by the
+# previous one. That factor is then being passed to scale_frames.main() for further
+# computation and changes to the keyframe values.
+
 
 MAYA_WINDOW_POINTER = omui.MQtUtil.mainWindow()
 MAYA_MAIN_WINDOW = wrapInstance(long(MAYA_WINDOW_POINTER), QWidget)
@@ -23,7 +33,7 @@ class MainWindow(QWidget):
 
         self.is_selection = True
         self.is_first = True
-        self.vertical = True
+        self.is_vertical = True
 
         grid = QGridLayout(self)
         grid.setAlignment(Qt.AlignTop)
@@ -38,44 +48,33 @@ class MainWindow(QWidget):
         explanation_of_selection.setWordWrap(True)
 
         # Horizontal and Vertical buttons determine whether we scale time-wise or value-wise
-        vertical_scale_button, horizontal_scale_button = self.add_toggle_buttons("V","H")
+        self.vertical_scale_button, self.horizontal_scale_button = self.add_toggle_buttons("V","H")
 
         # First and Last buttons determine the pivot from which the scaling is happening
-        first_frame_button, last_frame_button = self.add_toggle_buttons("F","L")
+        self.first_frame_button, self.last_frame_button = self.add_toggle_buttons("F","L")
 
         # Spin box determines how much are the keyframes getting scaled
-        spin_box = QDoubleSpinBox()
-        self.init_spin_box(spin_box)
+        self.spine_box = CustomSpinBox(value=1.0, parent=None, on_value_change = scale_frames.main,
+            is_first=self.first_frame_button.isChecked(),
+            are_selected_frames=self.is_selection,
+            is_vertical=self.is_vertical)
+        self.init_spin_box(self.spine_box)
 
-        # This button will be responsible for the main functionality
-        scale_button = QPushButton("Scale")
+        self.first_frame_button.clicked.connect(lambda: self.on_press_first())
+        self.last_frame_button.clicked.connect(lambda: self.on_press_last())
 
-        first_frame_button.clicked.connect(lambda: self.toggle_button(first_frame_button,
-                                                                      last_frame_button))
-        last_frame_button.clicked.connect(lambda: self.toggle_button(last_frame_button,
-                                                                     first_frame_button))
-
-        vertical_scale_button.clicked.connect(lambda: self.toggle_button(vertical_scale_button,
-                                                                         horizontal_scale_button))
-        horizontal_scale_button.clicked.connect(lambda: self.toggle_button(horizontal_scale_button,
-                                                                           vertical_scale_button))
+        self.vertical_scale_button.clicked.connect(lambda: self.on_press_vertical())
+        self.horizontal_scale_button.clicked.connect(lambda: self.on_press_horizontal())
 
         grid.addWidget(check_box, 0, 0, 1, 4)
         grid.addWidget(explanation_of_selection, 1, 0, 1, 4)
-        grid.addWidget(vertical_scale_button, 2, 1, 1, 1)
-        grid.addWidget(horizontal_scale_button, 2, 2, 1, 1)
-        grid.addWidget(first_frame_button, 3, 0, 1, 1)
-        grid.addWidget(spin_box, 3, 1, 1, 2)
-        grid.addWidget(last_frame_button, 3, 3, 1, 1)
-        grid.addWidget(scale_button, 4, 0, 1, 4)
+        grid.addWidget(self.vertical_scale_button, 2, 1, 1, 1)
+        grid.addWidget(self.horizontal_scale_button, 2, 2, 1, 1)
+        grid.addWidget(self.first_frame_button, 3, 0, 1, 1)
+        grid.addWidget(self.spine_box, 3, 1, 1, 2)
+        grid.addWidget(self.last_frame_button, 3, 3, 1, 1)
 
         check_box.stateChanged.connect(lambda: self.set_selection(check_box.isChecked()))
-
-        scale_button.clicked.connect(lambda: scale_frames.main(
-            is_first=first_frame_button.isChecked(),
-            are_selected_frames=self.is_selection,
-            scale_amount=spin_box.value(),
-            is_vertical=vertical_scale_button.isChecked()))
 
         self.show()
 
@@ -84,27 +83,34 @@ class MainWindow(QWidget):
         button.setDown(True)
         button.setChecked(True)
 
-    def init_spin_box(self, spin_box):
-        spin_box.setSingleStep(0.1)
-        spin_box.setMinimum(float("-inf"))
-        spin_box.setValue(0.5)
+    def init_spin_box(self, spine_box):
+        spine_box.setSingleStep(0.1)
+        spine_box.setMinimum(float("-inf"))
 
     def set_selection(self, is_selection):
         self.is_selection = is_selection
+        self.spine_box.is_selection = is_selection
 
-    def toggle_button(self, button, another_button):
-        """
-        Toggle for first and last buttons. Changes their down state depending on whether
-        they are checked
-        type: firstLast or horizontalVertical
-        """
-        if button.isChecked():
-            button.setDown(True)
-            if another_button.isChecked():
-                another_button.setChecked(False)
-                another_button.setDown(False)
-        else:
-            button.setDown(False)
+    def _toggle_button(self, pressed_button, unpressed_button):
+        pressed_button.setDown(True)
+        unpressed_button.setChecked(False)
+        unpressed_button.setDown(False)
+
+    def on_press_vertical(self):
+        self._toggle_button(self.vertical_scale_button, self.horizontal_scale_button)
+        self.spine_box.is_vertical = True
+
+    def on_press_horizontal(self):
+        self._toggle_button(self.horizontal_scale_button, self.vertical_scale_button)
+        self.spine_box.is_vertical = False
+
+    def on_press_first(self):
+        self._toggle_button(self.first_frame_button, self.last_frame_button)
+        self.spine_box.is_first = True
+
+    def on_press_last(self):
+        self._toggle_button(self.last_frame_button, self.first_frame_button)
+        self.spine_box.is_first = False
 
     def add_toggle_buttons(self, selected_label, unselected_label):
         """
@@ -116,8 +122,128 @@ class MainWindow(QWidget):
         button02.setCheckable(True)
         return button01, button02
 
+# This is taken from here, and modified:
+# https://stackoverflow.com/questions/20922836/increases-decreases-qspinbox-value-when-click-drag-mouse-python-pyside
+class CustomSpinBox(QLineEdit):
+    """
+    Tries to mimic behavior from Maya's internal slider that's found in the channel box.
+    """
+
+    IntSpinBox = 0
+    DoubleSpinBox = 1
+
+    def __init__(self, value=0, parent=None, on_value_change=None, **kwargs):
+        super(CustomSpinBox, self).__init__(parent)
+
+        self.setToolTip(
+            "Hold and drag middle mouse button to adjust the value\n"
+            "(Hold CTRL or SHIFT change rate)")
+
+        self.setValidator(QDoubleValidator(parent=self))
+
+        self.min = None
+        self.max = None
+        self.step = 0.1
+        self.value_at_press = None
+        self.pos_at_press = None
+        self.on_value_change_function = on_value_change
+        self.value_change_kwargs = kwargs
+        self.anim_curve_info_dict = {}
+
+        self.setValueText(value)
+        self.value = value
+        self.is_selection = self.value_change_kwargs["are_selected_frames"]
+        self.is_vertical = self.value_change_kwargs["is_vertical"]
+        self.is_first = self.value_change_kwargs["is_first"]
+
+        self.textChanged.connect(self.onTextChanged)
+
+    def onTextChanged(self, inserted_text):
+        if inserted_text != "":
+            new_value = float(inserted_text)
+            old_value = float(self.value)
+            if old_value!=0:
+                self.scale_factor = new_value/old_value
+            self.value = inserted_text
+
+            if self.on_value_change_function:
+                self.on_value_change_function(scale_factor=float(self.scale_factor),
+                                              are_selected_frames=self.is_selection,
+                                              is_vertical=self.is_vertical,
+                                              is_first=self.is_first)
+
+    def mousePressEvent(self, event):
+        mc.undoInfo(openChunk=True)
+        if event.buttons() == Qt.MiddleButton:
+            self.value_at_press = self.getValue()
+            self.pos_at_press = event.pos()
+            self.setCursor(QCursor(Qt.SizeHorCursor))
+        else:
+            super(CustomSpinBox, self).mousePressEvent(event)
+            self.selectAll()
+
+    def mouseReleaseEvent(self, event):
+        mc.undoInfo(closeChunk=True)
+        if event.button() == Qt.MiddleButton:
+            self.value_at_press = None
+            self.pos_at_press = None
+            self.setCursor(QCursor(Qt.IBeamCursor))
+            return
+
+        super(CustomSpinBox, self).mouseReleaseEvent(event)
+
+    def mouseMoveEvent(self, event):
+
+        if event.buttons() != Qt.MiddleButton:
+            return
+        if self.pos_at_press is None:
+            return
+
+        steps_mult = self.getStepsMultiplier(event)
+
+        delta = event.pos().x() - self.pos_at_press.x()
+        delta /= 6  # Make movement less sensitive.
+        delta *= self.step * steps_mult
+
+
+        value = self.value_at_press + delta
+        if value == 0:
+            value += self.step * steps_mult
+        self.setValueText(value)
+
+        super(CustomSpinBox, self).mouseMoveEvent(event)
+
+    def getStepsMultiplier(self, event):
+        steps_mult = 1
+
+        if event.modifiers() == Qt.CTRL:
+            steps_mult = 10
+        elif event.modifiers() == Qt.SHIFT:
+            steps_mult = 0.1
+
+        return steps_mult
+
+    def setSingleStep(self, step):
+        self.step = step
+
+    def getValue(self):
+        return float(self.text())
+
+    def setMinimum(self, min):
+        self.min = min
+
+    def setMaximum(self, max):
+        self.max = max
+
+    def setValueText(self, value):
+        if self.min is not None:
+            value = max(value, self.min)
+
+        if self.max is not None:
+            value = min(value, self.max)
+
+        self.setText(str(float(value)))
+
 
 def main():
     MainWindow()
-
-
